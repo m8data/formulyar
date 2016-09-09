007620ce43585ae4aec9793cd7421dbd0ed9e82e
Khubutiya A <ru7701@gmail.com>
2016-09-09 13:03:59 +0300

    пре-альфа релиз 0.0.4

--- pl/reg.pl
+++ pl/reg.pl
@@ -134,7 +134,7 @@ if ( $ARGV[0] ){
 	}
 	
 	if ( &getAvatar(1) ){ rmdir $planDir.'/'.$startAvatar if -d $planDir.'/'.$startAvatar }#здесь нельзя удалять через rmtree
-	else { symlink( $disk.$ROOT_DIR.$planDir.'/formulyar/doc/'.$startAvatar => $disk.$ROOT_DIR.$planDir.'/'.$startAvatar ) }
+	else { symlink( $disk.$ROOT_DIR.$planDir.'/formulyar/'.$startAvatar => $disk.$ROOT_DIR.$planDir.'/'.$startAvatar ) }
 	-d $auraDir || make_path( $auraDir, { chmod => $chmod } );
 	-d $auraDir.'/m8' || symlink( $disk.$ROOT_DIR.'m8' => $disk.$ROOT_DIR.$auraDir.'/m8' );
 	my @ava = &getDir( $planDir, 1 );
